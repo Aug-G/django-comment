f651e3f76e3cd4c065a9c827b8d5bafe1b5135c8
guohui <guohui_os@sari.ac.cn>
2015-10-13 14:16:38 +0800

    [FIX] wsgi bug

--- comments/wsgi_websocket.py
+++ comments/wsgi_websocket.py
@@ -1,9 +1,7 @@
-import gevent.monkey
+from gevent import socket
 import redis.connection
 import os
 
-
-
-redis.connection.socket = gevent.socket
+redis.connection.socket = socket
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 application = uWSGIWebsocketServer()
